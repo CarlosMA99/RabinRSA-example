fa79a8b54c59d9dc362a1cc6d4f3f878522745b6
Carlos Morán Alfonso <43845101+CarlosMA99@users.noreply.github.com>
2022-01-14 12:42:23 +0100

    Casi firma y errores corregidos

--- rsa.py
+++ rsa.py
@@ -1,12 +1,9 @@
 import random
-from utils.miller_rabin import es_primo_miller_rabin as mr_test
-from utils.expo_rapida import expo_rapida as pow_ef
-from utils.euclides_extendido import euclides_gcd as euclid_gcd
-from utils.euclides_extendido import euclides_extendido as euclid_ext
+import utils as ut
 
 LDB = 300
 UDB = 500
-CARD = 127
+CARD = 257
 
 def create_prime(digs):
     impares = [1,3,7,9]
@@ -17,7 +14,7 @@ def create_prime(digs):
         pot *= 10
     num += (random.randint(1,9))*pot
 
-    while num % 4 == 1 or not mr_test(num):
+    while num % 4 == 1 or not ut.es_primo_miller_rabin(num):
         digs = random.randint(LDB,UDB)
         pot = 10
         num = random.choice(impares)
@@ -41,13 +38,17 @@ class RSA:
         self.p1 = create_prime(digs1)
         digs2 = random.randint(LDB,UDB)
         self.p2 = create_prime(digs2)
-        self.lim = 10**(digs1 + digs2 - 2)
+        if self.p1 < self.p2:
+            self.p1 , self.p2 = self.p2, self.p1
+        self.lim = ut.expo_rapida(10,digs1 + digs2 - 1)
         self.n = self.p1*self.p2
         self.tot = (self.p1-1)*(self.p2-1)
         self.e = random.randint(2,self.tot-1)
-        while euclid_gcd(self.e,self.tot) > 1:
+        bezout = ut.euclides_extendido(self.tot,self.e)
+        while bezout[2] > 1:
             self.e = random.randint(2,self.tot-1)
-        self.d = euclid_ext(self.e,self.tot)[0]
+            bezout = ut.euclides_extendido(self.tot,self.e)
+        self.d = bezout[1]
         self.publica = (self.n,self.e)
         self.privada = (self.d,self.p1,self.p2)
 
@@ -70,6 +71,8 @@ class RSA:
             pot = 1
             while pot < lim and i < len(s):
                 ascii_ind = ord(s[i])+1
+                if ascii_ind >= 1000:
+                    print(ascii_ind)
                 curr += (ascii_ind*pot)
                 pot *= 1000
                 i += 1
@@ -80,7 +83,7 @@ class RSA:
         s = ""
         while cipher > 0:
             ch = cipher%1000
-            if ch > 128:
+            if ch > CARD:
                 return
             s += chr(ch-1)
             cipher //= 1000
@@ -90,15 +93,14 @@ class RSA:
         (n,e) = dest.public_key()
         lim = dest.get_lim()
         cipher = self.humanToCipher(lim,mes)
-        print(cipher)
-        return [(pow_ef(x,2,n)) for x in cipher]
+        return [(ut.expo_rapida(x,2,n)) for x in cipher]
     
     def decrypt(self,cipher):
         texts = list()
         for n in cipher:
-            sq_modp = pow_ef(n,(self.p1+1)//4,self.p1)
-            sq_modq = pow_ef(n,(self.p2+1)//4,self.p2)
-            (x1,y1) = euclid_ext(self.p1,self.p2)
+            sq_modp = ut.expo_rapida(n,(self.p1+1)//4,self.p1)
+            sq_modq = ut.expo_rapida(n,(self.p2+1)//4,self.p2)
+            (x1,y1,mcd) = ut.euclides_extendido(self.p1,self.p2)
             r1 = (self.p1*sq_modq*x1+self.p2*sq_modp*y1)%self.n
             r2 = self.n - r1
             r3 = (self.p1*sq_modq*x1-self.p2*sq_modp*y1)%self.n
@@ -109,8 +111,6 @@ class RSA:
             for r in roots:
                 frag.append(self.cipherToHuman(r))
             texts.append(frag)
-        
-        print(texts)
         
         for i in range(len(texts[0])):
             text = ""
@@ -127,38 +127,76 @@ class RSA:
 
         return
 
+    #Usuario con clave pública (nA, eA) y privada (pA, qA, dA)
+    #Firma a un usuario con clave pública (nB , eB) y privada (pB, qB, dB)
+    #El mensaje M ∈ Z/<N^k>
+        
+    def firmar_con_privacidad(self,dest):
+        menfirma = "Firmado"
+        (nD,eD) = dest.public_key()
+        nE, eE, dE = self.n, self.e, self.d
+        lim = min(self.lim,dest.get_lim())
+        #Enviar C
+        c = list()
+        m = self.humanToCipher(lim,menfirma)
+        if nE < nD:
+            for elem in m:
+                c.append(ut.expo_rapida(ut.expo_rapida(elem,dE,nE), eD, nD))
+        else:
+            for elem in m:
+                c.append(ut.expo_rapida(ut.expo_rapida(elem,eD,nD), dE, nE))
+        return c
+
     
+    def verificar_firma(self,em,m):
+        (nE,eE) = em.public_key()
+        nD, eD, dD = self.n, self.e, self.d
+        men = list()
+        if nE < nD:
+            for elem in m:
+                men.append(ut.expo_rapida(ut.expo_rapida(elem,dD,nD), eE, nE))
+        else:
+            for elem in m:
+                men.append(ut.expo_rapida(ut.expo_rapida(elem,eE,nE), dD, nD))
+        menfirma = ""
+        for elem in men:
+            menfirma += self.cipherToHuman(elem)
+        print(menfirma)
+        if menfirma == "Firmado":
+            print("Firma válida\n")
+            return True
+        print("Firma no válida\n")
+        return False
+        
+
     def send_message(self,dest,men):
-        encr = self.encrypt(dest,men)
-        print("Mensaje de {0} a {1}: Mensaje enviado: {2}".format(self.id,dest.get_id(),men))
-        return encr
+        return [self.encrypt(dest,men),self.firmar_con_privacidad(dest)] 
 
 
     def rec_message(self,sen,cif):
-        decr = self.decrypt(cif)
-        print("Mensaje de {0} a {1}: Mensaje recibido: {2}".format(sen.get_id(),self.id,decr))
+        decr = self.decrypt(cif[0])
+        self.verificar_firma(sen,cif[1])
         return decr
-
 
 
 Alice = RSA("Alice")
 Bob = RSA("Bob")
 m1 = "Hola que tal"
 m2 = "Muy bien"
-m3 = "Codigos: a123421328910asfjkkljrijnij ijasirjijweqfdjasoirjqf184u312398jf"
+m3 = "Códigos: ñóáéíóúa123421328910asfjkkljrijnij ijasirjijweqfdjasoirjqf184u312398jf"
 m4 = "Ya han sido insertados"
 
 en1 = Alice.send_message(Bob,m1)
-Bob.rec_message(Alice,en1)
+dec1 = Bob.rec_message(Alice,en1)
 
 en2 = Bob.send_message(Alice,m2)
-Alice.rec_message(Bob,en2)
+dec2 = Alice.rec_message(Bob,en2)
 
 en3 = Alice.send_message(Bob,m3)
-Bob.rec_message(Alice,en3)
+dec3 = Bob.rec_message(Alice,en3)
 
 en4 = Bob.send_message(Alice,m4)
-Alice.rec_message(Bob,en4)
+dec4 = Alice.rec_message(Bob,en4)
 
 
 
---
+++ utils.py
@@ -0,0 +1,96 @@
+import random
+
+
+
+def expo_rapida(b,e,n=0): # Función que calcula b^e mod n de manera eficiente. Si n=0 (por defecto) calcula b^e
+    if e == 0:
+        return 1
+    if n > 0:    
+        b %= n
+    res = 1
+    while e > 0:
+        if e%2 == 1:
+            res = (res*b)
+            if n > 0:
+                res %= n
+        b = (b*b)
+        if n > 0:
+            b %= n
+        e //= 2
+    return res
+
+
+def euclides_gcd(a,b):
+    if a < b:
+        a, b = b, a
+    if a == 0 or b == 0:
+        return a
+    rem = a%b
+    while rem > 0:
+        a,b,rem = b,rem,b%rem
+    return b
+
+def euclides_extendido(a,b): # Función que calcula b^e mod n de manera eficiente. Si n=0 (por defecto) calcula b^e
+    if a < b:
+        a, b = b, a
+    if a == 0 or b == 0:
+        return a
+
+    aux_a = a
+    aux_b = b
+
+    seq1 = [1,0]
+    seq2 = [0,1]
+
+    rem = a%b
+    while rem > 0:
+        q = a//b
+        k = len(seq1)
+        seq1.append(seq1[k-1]*q+seq1[k-2])
+        seq2.append(seq2[k-1]*q+seq2[k-2])
+        a,b,rem = b,rem,b%rem
+    
+    k = len(seq1)
+    (p,q) = (seq1[k-1]*(-1)**k,seq2[k-1]*(-1)**(k+1))
+
+    if p*aux_a+q*aux_b < 0:
+        p, q = -p, -q
+    
+    return (p,q,b)
+
+
+def descomponer_en_impar(prim):
+    i = 0
+    while prim%2 == 0:
+        i += 1
+        prim = prim // 2
+    return (i,prim)
+
+
+def miller_rabin(n):
+    if n == 2 or n == 3:
+        return True
+    if n == 1 or n%2 == 0:
+        return False
+
+    a = random.randint(2,n-2)
+    (pot,num) = descomponer_en_impar(n-1)
+
+    res = expo_rapida(a,num,n)
+    if res == 1 or res == n-1:
+        return True
+    
+    for i in range(pot-1):
+        res = (res*res)%n
+        if res == 1:
+            return False
+        if res == n-1:
+            return True
+    return False
+
+
+def es_primo_miller_rabin(n,k=10):
+    for i in range(k):
+        if not miller_rabin(n):
+            return False
+    return True
