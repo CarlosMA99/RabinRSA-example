f7b3adf7a2dd9ce3acd67fd40dc3d42dd5298c34
paloza01 <79474124+paloza01@users.noreply.github.com>
2022-01-14 01:08:20 +0100

    Update firmas.py

--- firmas.py
+++ firmas.py
@@ -1,16 +1,18 @@
+from utils.expo_rapida import expo_rapida as pow_ef
+
 #Usuario con clave pública (nA, eA) y privada (pA, qA, dA)
 #Firma a un usuario con clave pública (nB , eB) y privada (pB, qB, dB)
 #El mensaje M ∈ Z/<N^k>
 
 def firma_digital_sin_privacidad():
-    C = expo_rapida(M,dA,nA)
+    C = pow_ef(M,dA,nA)
     #Enviar C
-    if (expo_rapida(C,eA,nA) == M)
+    if (pow_ef(C,eA,nA) == M)
     
 def firma_digital_con_privacidad():
-    C = expo_rapida(M,dA,nA)
+    C = pow_ef(M,dA,nA)
     #Enviar C
     if (nA < nB):
-        C = expo_rapida(expo_rapida(M,dA,nA)), eB, nB)
+        C = pow_ef(pow_ef(M,dA,nA)), eB, nB)
     else:
-        C = expo_rapida(expo_rapida(M,eB,nB)), dA, nA)      
+        C = pow_ef(pow_ef(M,eB,nB)), dA, nA)      
